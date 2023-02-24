d5fe14805319219d9a4a9100a03471f56c6c4818
Calin <acalin729@gmail.com>
2023-02-24 21:13:09 +0200

    rolf

--- app/routes.py
+++ app/routes.py
@@ -9,15 +9,29 @@ def home():
 
 
 @app.route('/login')
-def login_register():
+def login_page():
     return render_template('login.html')
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'admin' and request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('home'))
     return render_template('login.html', error = error)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' and request.form['password'] != 'admin' and request.form['email'] != 'admin@admin.com':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('register.html', error = error)
