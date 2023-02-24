284fd1510ecbef3506e8cf7d15c9cbe435000c2f
David Seu 916 <david.seu@stud.ubbcluj.ro>
2023-02-24 17:10:18 +0200

    rolf

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,11 @@ from flask import render_template
 
 
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/home')
+def home():
     return render_template('index.html')
+
+
+@app.route('/login-register')
+def login_register():
+    return render_template('login-register.html')
