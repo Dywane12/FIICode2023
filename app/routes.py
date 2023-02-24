7329314800d3540f5dba4cb26d85285118332361
Calin <acalin729@gmail.com>
2023-02-24 18:08:51 +0200

    rolf

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 
 @app.route('/')
@@ -11,3 +11,13 @@ def home():
 @app.route('/login-register')
 def login_register():
     return render_template('login-register.html')
+
+@app.route('/login-register', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login-register.html', error = error)
