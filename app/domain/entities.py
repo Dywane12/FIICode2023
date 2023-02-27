c3280e06671d634adbccf2507981d8a0935bf4bf
David Seu 916 <david.seu@stud.ubbcluj.ro>
2023-02-27 17:02:55 +0200

    rolf

--- app/domain/entities.py
+++ app/domain/entities.py
@@ -15,6 +15,7 @@ class Patient(db.Model):
     gender = db.Column(db.String(8), index=True)
     medical_record_id = db.Column(db.Integer, index=True, unique=True)
     doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'))
+    consultation = db.relationship('Consultation', backref='patient', lazy='dynamic')
 
     def __init__(self, username, first_name, last_name, phone_number, email, address, serie_buletin, cnp,
                  birth_date, marital_status, gender, medical_record_id, doctor_id):
@@ -32,6 +33,7 @@ class Patient(db.Model):
         self.medical_record_id = medical_record_id
         self.doctor_id = doctor_id
 
+
 class Doctor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -45,7 +47,8 @@ class Doctor(db.Model):
     consultation_schedule_office = db.Column(db.String(128), index=True)
     consultation_schedule_away = db.Column(db.String(128), index=True)
     assistants_schedule = db.Column(db.String(128), index=True)
-    patients = db.relationship('Patient', backref='Fullname', lazy='dynamic')
+    patients = db.relationship('Patient', backref='doctor', lazy='dynamic')
+    consultations = db.relationship('Consultation', backref='doctor', lazy='dynamic')
 
     def __init__(self, username,  first_name, last_name, phone_number, email, address, birth_date,
                  gender, consultation_schedule_office, consultation_schedule_away, assistants_schedule):
