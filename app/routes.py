eefe615c3eb6f5bd4ddf39af0c45377ab85aa071
Calin <acalin729@gmail.com>
2023-02-28 22:52:03 +0200

    rolf

--- app/routes.py
+++ app/routes.py
@@ -37,7 +37,7 @@ def register_medic():
         if request.form['username'] != 'admin' and request.form['password'] != 'admin' and request.form['email'] != 'admin@admin.com':
             error = 'Date gresite. Incearca din nou.'
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for('medic_home'))
     return render_template('register_medic.html', error = error)
 
 @app.route('/register-pacient', methods=['GET', 'POST'])
@@ -53,3 +53,23 @@ def register_pacient():
 @app.route('/choice')
 def choice():
     return render_template('choice.html')
+
+@app.route('/medic-home')
+def medic_home():
+    return render_template('principal-medic.html')
+
+@app.route('/lista-pacienti')
+def lista_pacienti():
+    return render_template('lista-pacienti.html')
+
+@app.route('/transfer-pacienti')
+def transfer_pacienti():
+    return render_template('transfer-pacienti.html')
+
+@app.route('/invita-pacienti')
+def invita_pacienti():
+    return render_template('invita-pacienti.html')
+
+@app.route('/medic-profil')
+def medic_profil():
+    return render_template('medic-profil.html')
