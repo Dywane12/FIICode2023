b44c2067dbb105339ead9268cba78a7a4a3ca04c
David Seu 916 <david.seu@stud.ubbcluj.ro>
2023-03-03 13:20:37 +0200

    rolf

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from config import Config
+from app.config import Config
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,12 +10,11 @@ convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__, template_folder='/ui')
 app.config.from_object(Config)
 metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(app, metadata=metadata)
 migrate = Migrate(app, db, render_as_batch=True)
 
-
-from app import routes
+from app.ui import routes
 from app.domain.entities import Patient, Doctor, Consultation
