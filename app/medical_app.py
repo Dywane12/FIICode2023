9e3f00ccd350f200f4fe04cebe39f70536ccd6a5
David Seu 916 <david.seu@stud.ubbcluj.ro>
2023-03-15 12:27:58 +0200

    updated tests

--- app/medical_app.py
+++ app/medical_app.py
@@ -10,4 +10,3 @@ if __name__ == '__main__':
     @app.shell_context_processor
     def make_shell_context():
         return {'db': db, 'Patient': Patient, 'Doctor': Doctor, 'Consultation': Consultation}
- #
